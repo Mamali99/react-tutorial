59654defe1098e2567dbf13e55f491c12f5f6806
Mohammad Ghalandari <mohammadghalandari1998@gmail.com>
2025-05-16 09:18:58 +0200

    project started

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Hello from "./components/Hello";
+function App() {
+  return <>
+    <Hello />
+    <h1>test</h1>
+  </>;
+}
+
+export default App
---
+++ src/components/Hello.jsx
@@ -0,0 +1,4 @@
+function Hello() {
+  return <h1>Hello from Hello-component</h1>;
+}
+export default Hello;
